3d520a45ea494f1d683166c7d6a21f0625be31f2
Siddarth Kalra <sid@sidkalra.com>
2016-11-20 16:29:46 -0500

    HTTPClient now supports a new public method: requestData()

--- PhotoBrowser500px/HTTPClient.swift
+++ PhotoBrowser500px/HTTPClient.swift
@@ -16,6 +16,11 @@ class HTTPClient {
         let error: HTTPResponseError?
     }
     
+    public struct HTTPDataResponse {
+        let data: Data?
+        let error: HTTPResponseError?
+    }
+    
     enum HTTPResponseError: Error {
         case noConnection
         case dataEmpty
@@ -23,10 +28,50 @@ class HTTPClient {
         case validationFailure(internalError: Error)
     }
     
-    class func request(_ url: String,
-                 method: HTTPMethod = .get,
-                 parameters: [String: Any]? = nil,
-                 completionHandler: @escaping (HTTPResponse) -> Void)
+    // Returns binary data
+    class func requestData(_ url: URL,
+                           parameters: [String: Any]? = nil,
+                           completionHandler: @escaping (HTTPDataResponse) -> Void)
+    {
+        if !Reachability.isConnectedToNet() {
+            let error = HTTPResponseError.noConnection
+            completionHandler(HTTPDataResponse(data: nil, error: error))
+            
+            return
+        }
+        
+        Alamofire.request(url, method: .get, parameters: parameters).validate().responseData { (response: DataResponse<Data>) in
+            switch response.result {
+            case .success:
+                debugPrint("Validation Successful")
+                
+                if let data = response.result.value {
+                    completionHandler(HTTPDataResponse(data: data, error: nil))
+                }
+                else {
+                    // failed
+                    let error = HTTPResponseError.dataEmpty
+                    completionHandler(HTTPDataResponse(data: nil, error: error))
+                }
+                
+                break
+            case .failure(let error):
+                debugPrint(response.request!)
+                debugPrint(response.response!)
+                debugPrint(error)
+                let error = HTTPResponseError.validationFailure(internalError: error)
+                completionHandler(HTTPDataResponse(data: nil, error: error))
+                
+                break
+            }
+        }
+    }
+    
+    // Returns JSON
+    class func request(_ url: URL,
+                       method: HTTPMethod = .get,
+                       parameters: [String: Any]? = nil,
+                       completionHandler: @escaping (HTTPResponse) -> Void)
     {
         if !Reachability.isConnectedToNet() {
             let error = HTTPResponseError.noConnection
@@ -57,10 +102,14 @@ class HTTPClient {
                     let error = HTTPResponseError.dataEmpty
                     completionHandler(HTTPResponse(data: nil, error: error))
                 }
+                
+                break
             case .failure(let error):
                 debugPrint(error)
                 let error = HTTPResponseError.validationFailure(internalError: error)
                 completionHandler(HTTPResponse(data: nil, error: error))
+                
+                break
             }
         }
     }
