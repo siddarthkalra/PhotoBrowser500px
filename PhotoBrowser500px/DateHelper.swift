0455ebf687aba213484dacd1ec3a61c1f891a976
Siddarth Kalra <sid@sidkalra.com>
2016-11-20 12:01:44 -0500

    Added methods to DateHelper

--- PhotoBrowser500px/DateHelper.swift
+++ PhotoBrowser500px/DateHelper.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+class DateHelper {
+    
+    class func dateFromString(_ string: String, withDateFormat format: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+
+        return dateFormatter.date(from: string)!
+    }
+    
+    class func dateFromISO8601String(_ string: String) -> Date {
+        return DateHelper.dateFromString(string, withDateFormat: "yyyy-MM-dd'T'HH:mm:ssZZ")
+    }
+    
+    class func stringFromDate(date: NSDate) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        return dateFormatter.string(from: date as Date)
+    }
+    
+    class func stringFromDate(date: NSDate, withDateFormat format: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        
+        return dateFormatter.string(from: date as Date)
+    }
+    
+    class func stringFromDate(date: Date, withDateFormat format: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    class func datesAreOnSameDay(date1: Date, date2: Date) -> Bool {
+        let userCalender: Calendar = Calendar.current
+        return userCalender.compare(date1, to: date2, toGranularity: .day) == .orderedSame
+    }
+}
