2d5ba12a6cea562755ac0b975e7b1c6288bff462
Siddarth Kalra <sid@sidkalra.com>
2016-11-23 15:31:36 -0500

    Image details VC's scroll view now adjusts its constraints according to device orientation.

--- PhotoBrowser500px/ImageDetailViewController.swift
+++ PhotoBrowser500px/ImageDetailViewController.swift
@@ -19,7 +19,6 @@ class ImageDetailViewController: UIViewController, UIScrollViewDelegate {
     static let TAG_LABEL_NAME = 2
     static let TAG_LABEL_VIEW_COUNT = 3
     static let TAG_LABEL_VOTE_COUNT = 4
-    static let TAG_SCROLL_VIEW = 5
     
     // MARK: - Public Members
     
@@ -32,8 +31,116 @@ class ImageDetailViewController: UIViewController, UIScrollViewDelegate {
     // MARK: - Private Members
     
     private var innerScrollViewImages: [UIScrollView: UIImageView] = [:]
+    @IBOutlet weak var outerScrollView: UIScrollView!
+    
+    lazy private var scrollViewTop: NSLayoutConstraint = {
+        let topConstraint: NSLayoutConstraint = NSLayoutConstraint(item: self.outerScrollView,
+                                                                   attribute: .top,
+                                                                   relatedBy: .equal,
+                                                                   toItem: self.view,
+                                                                   attribute: .top,
+                                                                   multiplier: 1.0,
+                                                                   constant: 0.0)
+        
+        self.view.addConstraint(topConstraint)
+        return topConstraint
+    }()
+    
+    lazy private var scrollViewBottom: NSLayoutConstraint = {
+        let bottomConstraint: NSLayoutConstraint = NSLayoutConstraint(item: self.outerScrollView,
+                                                                      attribute: .bottom,
+                                                                      relatedBy: .equal,
+                                                                      toItem: self.view,
+                                                                      attribute: .bottom,
+                                                                      multiplier: 1.0,
+                                                                      constant: 0.0)
+        
+        self.view.addConstraint(bottomConstraint)
+        return bottomConstraint
+    }()
+    
+    lazy private var scrollViewCenterX: NSLayoutConstraint = {
+        let centerXConstraint: NSLayoutConstraint = NSLayoutConstraint(item: self.outerScrollView,
+                                                                       attribute: .centerX,
+                                                                       relatedBy: .equal,
+                                                                       toItem: self.view,
+                                                                       attribute: .centerX,
+                                                                       multiplier: 1.0,
+                                                                       constant: 0.0)
+        
+        self.view.addConstraint(centerXConstraint)
+        return centerXConstraint
+    }()
+    
+    lazy private var scrollViewLeading: NSLayoutConstraint = {
+        let leadingConstraint: NSLayoutConstraint = NSLayoutConstraint(item: self.outerScrollView,
+                                                                       attribute: .leading,
+                                                                       relatedBy: .equal,
+                                                                       toItem: self.view,
+                                                                       attribute: .leading,
+                                                                       multiplier: 1.0,
+                                                                       constant: 0.0)
+        
+        self.view.addConstraint(leadingConstraint)
+        return leadingConstraint
+    }()
+    
+    lazy private var scrollViewTrailing: NSLayoutConstraint = {
+        let trailingConstraint: NSLayoutConstraint = NSLayoutConstraint(item: self.outerScrollView,
+                                                                        attribute: .trailing,
+                                                                        relatedBy: .equal,
+                                                                        toItem: self.view,
+                                                                        attribute: .trailing,
+                                                                        multiplier: 1.0,
+                                                                        constant: 0.0)
+        
+        self.view.addConstraint(trailingConstraint)
+        return trailingConstraint
+    }()
+    
+    lazy private var scrollViewCenterY: NSLayoutConstraint = {
+        let centerYConstraint: NSLayoutConstraint = NSLayoutConstraint(item: self.outerScrollView,
+                                                                       attribute: .centerY,
+                                                                       relatedBy: .equal,
+                                                                       toItem: self.view,
+                                                                       attribute: .centerY,
+                                                                       multiplier: 1.0,
+                                                                       constant: 0.0)
+        
+        self.view.addConstraint(centerYConstraint)
+        return centerYConstraint
+    }()
+    
+    lazy private var scrollViewAspectRatio: NSLayoutConstraint = {
+        let aspect: NSLayoutConstraint = NSLayoutConstraint(item: self.outerScrollView,
+                                                            attribute: .width,
+                                                            relatedBy: .equal,
+                                                            toItem: self.outerScrollView,
+                                                            attribute: .height,
+                                                            multiplier: 1.0,
+                                                            constant: 0.0)
+        
+        self.outerScrollView.addConstraint(aspect)
+        return aspect
+    }()
     
     // MARK: - Private Methods
+    
+    private func setupOuterScrollViewConstraints() {
+        let orientation = UIApplication.shared.statusBarOrientation
+        
+        switch orientation {
+        case .landscapeLeft, .landscapeRight:
+            debugPrint("landscape")
+            NSLayoutConstraint.deactivate([self.scrollViewLeading, self.scrollViewTrailing, self.scrollViewCenterY])
+            NSLayoutConstraint.activate([self.scrollViewTop, self.scrollViewBottom, self.scrollViewCenterX, self.scrollViewAspectRatio])
+            break
+        default:
+            NSLayoutConstraint.deactivate([self.scrollViewTop, self.scrollViewBottom, self.scrollViewCenterX])
+            NSLayoutConstraint.activate([self.scrollViewLeading, self.scrollViewTrailing, self.scrollViewCenterY, self.scrollViewAspectRatio])
+            break
+        }
+    }
     
     private func setupInnerScrollView(outerScrollView: UIScrollView,
                                       image: UIImage,
@@ -167,23 +274,23 @@ class ImageDetailViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       
+        
+        self.setupOuterScrollViewConstraints()
+        
         // setup outer scrollview
-        if let scrollView = self.view.viewWithTag(ImageDetailViewController.TAG_SCROLL_VIEW) {
-            let actualScrollView = scrollView as! UIScrollView
-            actualScrollView.isPagingEnabled = true
-            
-            let firstInnerScrollViewInfo = self.setupInnerScrollView(outerScrollView: actualScrollView, image: self.detailImage!)
-            self.innerScrollViewImages[firstInnerScrollViewInfo.scrollView] = firstInnerScrollViewInfo.imageView
-            
-            let secondDetailImageInfo = self.imageResults?[self.detailImageIndex! + 1]
-            if let secondDetailImage = self.imageFetcher?.cache[URL(string: (secondDetailImageInfo?.imageURL)!)!] {
-                let secondInnerScrollViewInfo = self.setupInnerScrollView(outerScrollView: actualScrollView,
+        self.outerScrollView.isPagingEnabled = true
+        
+        let firstInnerScrollViewInfo = self.setupInnerScrollView(outerScrollView: self.outerScrollView, image: self.detailImage!)
+        self.innerScrollViewImages[firstInnerScrollViewInfo.scrollView] = firstInnerScrollViewInfo.imageView
+        
+        let secondDetailImageInfo = self.imageResults?[self.detailImageIndex! + 1]
+        if let secondDetailImage = self.imageFetcher?.cache[URL(string: (secondDetailImageInfo?.imageURL)!)!] {
+            let secondInnerScrollViewInfo = self.setupInnerScrollView(outerScrollView: self.outerScrollView,
                                                                       image: secondDetailImage,
                                                                       prevInnerScrollView: firstInnerScrollViewInfo.scrollView,
                                                                       isLast: true)
-                self.innerScrollViewImages[secondInnerScrollViewInfo.scrollView] = secondInnerScrollViewInfo.imageView
-            }
-            
+            self.innerScrollViewImages[secondInnerScrollViewInfo.scrollView] = secondInnerScrollViewInfo.imageView
         }
 
         if let nameLabel = self.view.viewWithTag(ImageDetailViewController.TAG_LABEL_NAME) {
@@ -210,6 +317,20 @@ class ImageDetailViewController: UIViewController, UIScrollViewDelegate {
         if let voteCountLabel = self.view.viewWithTag(ImageDetailViewController.TAG_LABEL_VOTE_COUNT) {
             (voteCountLabel as! UILabel).text = "\(detailImageInfo.votesCount) votes"
         }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate(alongsideTransition: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            for scrollView in self.innerScrollViewImages.keys {
+                scrollView.zoomScale = 1.0
+            }
+            
+            self.setupOuterScrollViewConstraints()
+        }, completion: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            debugPrint("rotation completed")
+        })
+        
+        super.viewWillTransition(to: size, with: coordinator)
     }
     
     // MARK: - UIScrollViewDelegate
